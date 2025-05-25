1f5557f18a01b189409b04c8ac46d90fb7e115a2
DESKTOP-684DG1A\SeungJu <skatmdwn23@naver.com>
2025-05-25 19:21:27 +0900

    미니 프로젝트2 생성

---
+++ src/main/java/org/minisecond/project/MiniSecondApplication.java
@@ -0,0 +1,13 @@
+package org.minisecond.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniSecondApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/minisecond/project/MiniSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package org.minisecond.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
